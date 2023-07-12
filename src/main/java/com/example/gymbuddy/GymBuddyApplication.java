f3c47cd3dd8856233caf53044125bebc3c3ed569
lucas-buccilli <lucas.buccilli@willowtreeapps.com>
2023-07-12 13:46:57 -0400

    Initial commit

---
+++ src/main/java/com/example/gymbuddy/GymBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.example.gymbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymBuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymBuddyApplication.class, args);
+    }
+
+}
