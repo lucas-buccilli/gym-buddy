88321aa058702847bff3a6e5484534afaee6ade2
ivash1228 <ivash1228@gmail.com>
2023-07-19 14:03:36 -0400

    Add buildscript, fix migration file

--- src/main/resources/db/V1__machines_create.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE 'machines' (
-    'id' int NOT NULL AUTO_INCREMENT,
-    'name' varchar(500) NOT NULL
-)
---
+++ src/main/resources/db/migration/V1_1__machines_create.sql
@@ -0,0 +1,4 @@
+CREATE TABLE machines (
+    id int AUTO_INCREMENT primary key NOT NULL,
+    name varchar(500) NOT NULL
+)
